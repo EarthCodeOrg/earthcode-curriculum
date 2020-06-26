87522fbc08d8941eee25187d5e42db8e328c1df7
Karo Castro-wunsch <karoantonio@google.com>
2020-06-26 15:23:36 -0600

    next

---
+++ modules/m0001/m0001_basic_python_primer.py
@@ -0,0 +1,11 @@
+# Python Comments
+# this is a comment, it is a line of code that will not be executed
+
+'''
+using triple single quotes
+is a way of doing a multiline comments
+though technichally its a 
+multiline string that is not run by python
+'''
+
+
