830a55f52ffe6049d1e3079357dfd8acce87593b
Karo Castro-wunsch <karoantonio@google.com>
2020-06-26 15:56:52 -0600

    m0001

--- modules/m0001/m0001_basic_python_primer.py
+++ modules/m0001/m0001_basic_python_primer.py
@@ -1,11 +1,26 @@
+
+# Python Syntax
 # Python Comments
 # this is a comment, it is a line of code that will not be executed
 
 '''
-using triple single quotes
+Using triple single quotes
 is a way of doing a multiline comments
 though technichally its a 
 multiline string that is not run by python
 '''
+
+# Shell vs python file 
+# Variables
+# Basic Types and operators
+# Basic Operators
+# Boolean Logic
+# Strings
+# Functions
+# Tuples, Set, Lists, Dictionaries
+# Loops
+
+
+
 
 
